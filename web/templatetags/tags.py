39b5ef11a46e923d15908d717f360b957f059818
karpov-sv <karpov.sv@gmail.com>
2020-06-14 03:03:21 +0200

    Improvements for cutouts service

--- web/templatetags/tags.py
+++ web/templatetags/tags.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from django import template
 from django.utils.safestring import mark_safe
 
+from astropy.coordinates import SkyCoord
+
 from .. import views_cutouts
 
 register = template.Library()
@@ -24,3 +26,23 @@ def cat_row_ra(cat, row):
 @register.simple_tag(takes_context=False)
 def cat_row_dec(cat, row):
     return row[cat.get('dec', 'DEJ2000')]
+
+@register.simple_tag(takes_context=False)
+def galactic_l(ra, dec):
+    c = SkyCoord(ra, dec, unit='deg', frame='icrs')
+    return c.transform_to('galactic').l.to_string(decimal=True)
+
+@register.simple_tag(takes_context=False)
+def galactic_b(ra, dec):
+    c = SkyCoord(ra, dec, unit='deg', frame='icrs')
+    return c.transform_to('galactic').b.to_string(decimal=True, alwayssign=True)
+
+@register.simple_tag(takes_context=False)
+def eq_ra(ra, dec):
+    c = SkyCoord(ra, dec, unit='deg', frame='icrs')
+    return c.ra.to_string(unit='hour', sep=' ', pad=True)
+
+@register.simple_tag(takes_context=False)
+def eq_dec(ra, dec):
+    c = SkyCoord(ra, dec, unit='deg', frame='icrs')
+    return c.dec.to_string(sep=' ', pad=True, alwayssign=True)
--- web/views_cutouts.py
+++ web/views_cutouts.py
@@ -5,6 +5,8 @@ from django.core.cache import cache
 from django.urls import reverse
 from django.shortcuts import redirect
 
+from collections import OrderedDict
+
 import urllib
 from . import ps1
 
@@ -22,11 +24,52 @@ def cutouts_ps1(request, size=512):
 
     return redirect(url)
 
-cutout_surveys = ['galex_nuv', 'galex_fuv', 'dss', 'sdss', 'ps1', 'skymapper', 'wise_w1', 'wise_w2', 'wise_w3', 'wise_w4']
+# cutouts_direct = ['galex_nuv', 'galex_fuv', 'dss', 'sdss']
+
+surveys = OrderedDict([
+    ['galex', {'name': 'GALEX', 'hips': 'CDS/P/GALEXGR6/AIS/color', 'default':True}],
+
+    ['galex_nuv', {'name': 'GALEX NUV'}],
+    ['galex_fuv', {'name': 'GALEX FUV'}],
+
+    ['dss2', {'name': 'DSS2', 'hips': 'DSS2/color', 'default':True}],
+
+    ['sdss', {'name': 'SDSS DR12', 'default':True}],
+
+    ['panstarrs', {'name': 'PanSTARRS', 'hips': 'CDS/P/PanSTARRS/DR1/color', 'default':True}],
+
+    ['skymapper', {'name': 'SkyMapper', 'hips': 'CDS/P/skymapper-color', 'default':True}],
+
+    ['iphas_halpha', {'name': 'IPHAS Halpha', 'hips': 'CDS/P/IPHAS/DR2/halpha'}],
+
+    ['wise', {'name': 'allWISE', 'hips': 'CDS/P/allWISE/color', 'default':True}],
+    ['wise_w1', {'name': 'WISE 3.4um', 'hips': 'CDS/P/allWISE/W1'}],
+    ['wise_w2', {'name': 'WISE 4.6um', 'hips': 'CDS/P/allWISE/W2'}],
+    ['wise_w3', {'name': 'WISE 12um', 'hips': 'CDS/P/allWISE/W3'}],
+    ['wise_w4', {'name': 'WISE 22um', 'hips': 'CDS/P/allWISE/W4'}],
+
+])
 
 def get_cutout_url(survey, ra, dec, sr, size, overlay=False):
+    # generic HIPS
+    if surveys.has_key(survey) and surveys[survey].has_key('hips'):
+        return {'name': surveys[survey]['name'],
+                'url': 'http://alasky.u-strasbg.fr/hips-image-services/hips2fits?' + urllib.urlencode({
+                    'hips': surveys[survey]['hips'],
+                    'ra': ra,
+                    'dec': dec,
+                    'width': size,
+                    'height': size,
+                    'fov': sr,
+                    'projection': 'TAN',
+                    'coordsys': 'icrs',
+                    'rotation_angle': 0.0,
+                    'format': 'jpg',
+                    'stretch': 'linear',
+                    'cmap': 'viridis'})}
+
     # GALEX
-    if survey == 'galex_nuv':
+    elif survey == 'galex_nuv':
         return {'name': 'GALEX NUV',
                 'url': 'https://skyview.gsfc.nasa.gov/current/cgi/runquery.pl?' + urllib.urlencode({
                     'Position': '%s,%s' % (ra, dec),
@@ -46,21 +89,6 @@ def get_cutout_url(survey, ra, dec, sr, size, overlay=False):
                     'Catalog': 'II/335/galex_ais' if overlay else None,
                     'Return': 'GIF',
                     'LUT': 'colortables/blue-white.bin'})}
-    elif survey == 'galex':
-        return {'name': 'GALEX',
-                'url': 'http://alasky.u-strasbg.fr/hips-image-services/hips2fits?' + urllib.urlencode({
-                    'hips': 'CDS/P/GALEXGR6/AIS/color',
-                    'ra': ra,
-                    'dec': dec,
-                    'width': size,
-                    'height': size,
-                    'fov': sr,
-                    'projection': 'TAN',
-                    'coordsys': 'icrs',
-                    'rotation_angle': 0.0,
-                    'format': 'jpg',
-                    'stretch': 'linear'})}
-
     # DSS
     elif survey == 'dss':
         return {'name': 'DSS',
@@ -93,143 +121,72 @@ def get_cutout_url(survey, ra, dec, sr, size, overlay=False):
                     'sr': sr,
                     'size': size})}
 
-    elif survey == 'panstarrs' or survey == 'ps1':
-        return {'name': 'PanSTARRS',
-                'url': 'http://alasky.u-strasbg.fr/hips-image-services/hips2fits?' + urllib.urlencode({
-                    'hips': 'CDS/P/PanSTARRS/DR1/color',
-                    'ra': ra,
-                    'dec': dec,
-                    'width': size,
-                    'height': size,
-                    'fov': sr,
-                    'projection': 'TAN',
-                    'coordsys': 'icrs',
-                    'rotation_angle': 0.0,
-                    'format': 'jpg',
-                    'stretch': 'linear'})}
-
-    # WISE
-    elif survey == 'wise':
-        return {'name': 'allWISE',
-                'url': 'http://alasky.u-strasbg.fr/hips-image-services/hips2fits?' + urllib.urlencode({
-                    'hips': 'CDS/P/allWISE/color',
-                    'ra': ra,
-                    'dec': dec,
-                    'width': size,
-                    'height': size,
-                    'fov': sr,
-                    'projection': 'TAN',
-                    'coordsys': 'icrs',
-                    'rotation_angle': 0.0,
-                    'format': 'jpg',
-                    'stretch': 'linear'})}
-    elif survey == 'wise_w1':
-        return {'name': 'WISE 3.4um',
-                'url': 'http://alasky.u-strasbg.fr/hips-image-services/hips2fits?' + urllib.urlencode({
-                    'hips': 'CDS/P/allWISE/W1',
-                    'ra': ra,
-                    'dec': dec,
-                    'width': size,
-                    'height': size,
-                    'fov': sr,
-                    'projection': 'TAN',
-                    'coordsys': 'icrs',
-                    'rotation_angle': 0.0,
-                    'format': 'jpg',
-                    'stretch': 'linear',
-                    'cmap': 'viridis'})}
-    elif survey == 'wise_w2':
-        return {'name': 'WISE 4.6um',
-                'url': 'http://alasky.u-strasbg.fr/hips-image-services/hips2fits?' + urllib.urlencode({
-                    'hips': 'CDS/P/allWISE/W2',
-                    'ra': ra,
-                    'dec': dec,
-                    'width': size,
-                    'height': size,
-                    'fov': sr,
-                    'projection': 'TAN',
-                    'coordsys': 'icrs',
-                    'rotation_angle': 0.0,
-                    'format': 'jpg',
-                    'stretch': 'linear',
-                    'cmap': 'viridis'})}
-    elif survey == 'wise_w3':
-        return {'name': 'WISE 12um',
-                'url': 'http://alasky.u-strasbg.fr/hips-image-services/hips2fits?' + urllib.urlencode({
-                    'hips': 'CDS/P/allWISE/W3',
-                    'ra': ra,
-                    'dec': dec,
-                    'width': size,
-                    'height': size,
-                    'fov': sr,
-                    'projection': 'TAN',
-                    'coordsys': 'icrs',
-                    'rotation_angle': 0.0,
-                    'format': 'jpg',
-                    'stretch': 'linear',
-                    'cmap': 'viridis'})}
-    elif survey == 'wise_w4':
-        return {'name': 'WISE 22um',
-                'url': 'http://alasky.u-strasbg.fr/hips-image-services/hips2fits?' + urllib.urlencode({
-                    'hips': 'CDS/P/allWISE/W4',
-                    'ra': ra,
-                    'dec': dec,
-                    'width': size,
-                    'height': size,
-                    'fov': sr,
-                    'projection': 'TAN',
-                    'coordsys': 'icrs',
-                    'rotation_angle': 0.0,
-                    'format': 'jpg',
-                    'stretch': 'linear',
-                    'cmap': 'viridis'})}
-
-    # SkyMapper
-    elif survey == 'smss' or survey == 'skymapper':
-        return {'name': 'SkyMapper',
-                'url': 'http://alasky.u-strasbg.fr/hips-image-services/hips2fits?' + urllib.urlencode({
-                    'hips': 'CDS/P/skymapper-color',
-                    'ra': ra,
-                    'dec': dec,
-                    'width': size,
-                    'height': size,
-                    'fov': sr,
-                    'projection': 'TAN',
-                    'coordsys': 'icrs',
-                    'rotation_angle': 0.0,
-                    'format': 'jpg',
-                    'stretch': 'linear',
-                    'cmap': 'viridis'})}
 
     return None
 
 def cutouts(request, size=512):
-    context = {'sr': 0.01}
+    context = {'sr_value': 0.1, 'sr_units': 'deg'}
+
+    context['selected'] = [_ for _ in surveys.keys() if surveys[_].get('default')]
+    context['surveys'] = surveys
 
     if request.method == 'POST' or request.GET.get('coords'):
         # Form submission handling
 
-        coords = request.POST.get('coords', request.GET.get('coords'))
-        sr = float(request.POST.get('sr', request.GET.get('sr', 0.1)))
-
-        name,ra,dec = resolve(coords)
-
-        for _ in ['coords']:
+        for _ in ['coords', 'multicoords', 'sr_value', 'sr_units', 'sr']:
             context[_] = request.POST.get(_, request.GET.get(_))
 
-        if not name:
-            context['message'] = "Can't resolve position: " + coords
+        if context['sr']:
+            context['sr_value'] = context['sr']
+            context['sr_units'] = 'deg'
         else:
-            context['name'] = name
-            context['ra'] = ra
-            context['dec'] = dec
-            context['sr'] = float(sr) if sr else 0.1
+            context['sr'] = float(context['sr_value']) if context['sr_value'] else 0.01
+            if context['sr_units'] == 'arcmin':
+                context['sr'] /= 60
+            elif context['sr_units'] == 'arcsec':
+                context['sr'] /= 3600
 
-            context['cutouts'] = []
+        if request.POST.get('selected'):
+            context['selected'] = request.POST.getlist('selected')
 
-            for _ in cutout_surveys:
-                cut = get_cutout_url(_, ra, dec, sr, size)
-                if cut is not None:
-                    context['cutouts'].append(cut)
+        coords = request.POST.get('coords', request.GET.get('coords'))
+        multicoords = request.POST.get('multicoords')
+
+        if multicoords:
+            multicoords = multicoords.splitlines()
+
+        targets = []
+
+        clist = []
+        if coords:
+            clist += [coords]
+        if multicoords:
+            clist += multicoords
+
+        for coord in clist:
+            if not coord:
+                continue
+
+            target = {}
+
+            name,ra,dec = resolve(coord)
+
+            if not name:
+                target['message'] = "Can't resolve position: " + coord
+            else:
+                target['name'] = name
+                target['ra'] = ra
+                target['dec'] = dec
+
+                target['cutouts'] = OrderedDict()
+
+                for _ in surveys.keys():
+                    cut = get_cutout_url(_, ra, dec, context['sr'], size)
+                    if cut is not None:
+                        target['cutouts'][_] = cut
+
+            targets.append(target)
+
+        context['targets'] = targets
 
     return TemplateResponse(request, 'cutouts.html', context=context)
